0e44335fdc74ff2def6fb9d875d6d0c0bfce181f
hwong5208 <hwong5208@gmail.com>
2016-10-14 16:11:47 -0700

    fixed bugs

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -6,9 +6,10 @@
  */
 
 import {IInsightFacade, InsightResponse} from "./IInsightFacade";
-import {QueryRequest} from "./QueryController";
+import {QueryRequest, default as QueryController} from "./QueryController";
 import DatasetController from "./DatasetController";
 import Log from "../Util";
+import fs = require('fs');
 
 export default class InsightFacade implements IInsightFacade {
     private controller = new DatasetController();
@@ -43,14 +44,68 @@ export default class InsightFacade implements IInsightFacade {
 
     removeDataset(id: string): Promise<InsightResponse> {
 
+        let that = this;
+        return new Promise(function (fulfill,reject){
 
+            try {
 
+                if (that.controller.getDataset(id) != undefined){
+                    fs.unlinkSync("./data/"+id);
+                    that.controller.deleteDataSets(id);
+                    Log.trace('RouteHandler::deleteDataset(..) - processed');
+                    //res.json(204, {success: 'dataset is deleted'});
+                    fulfill({code:204,body: {success:'dataset is deleted'}});
+                    //console.log(" delete: 204")
+                } else {
+                   // res.json(404, {status: 'the delete was for a resource that was not previously PUT'});
+                    reject({code:404,body: {error:'the delete was for a resource that was not previously PUT'}});
+                    //console.log("delete: 404")
+                }
+            } catch (err) {
+                //Log.error('RouteHandler::deleteDataset(..) - ERROR: ' + err);
+                reject({code:400,body: {error:'400'}});
+                //res.send(400);
+            }
+        });
 
-        return undefined;
+       // return undefined;
     }
 
     performQuery(query: QueryRequest): Promise<InsightResponse> {
-        return undefined;
+
+        let that = this;
+        return new Promise(function (fulfill,reject){
+            try {
+                let datasets = that.controller.getDatasets();
+                let controller = new QueryController(datasets);
+                let isValid = controller.isValid(query);
+
+                if (isValid === true) {
+                    console.log("is valid true");
+                    let result = controller.query(query);
+                    if(result instanceof Array){ //res.json(424, {missing: result});
+                        reject({code:424,body: {missing: result}});
+                    }
+                    else {
+                       // res.json(200, result);
+                        fulfill({code:200,body: result});
+                    }
+                } else {
+                    console.log("is valid false");
+                    //res.json(400, {error: 'invalid query'});
+                    reject({code:400,body: {error:' invalid query'}});
+                }
+            } catch (err) {
+                Log.error('RouteHandler::postQuery(..) - ERROR: ' + err);
+               // res.send(400,{error: 'invalid query'});
+                reject({code:400,body: {error:' invalid query'}});
+            }
+
+        })
+
+
+
+      //  return undefined;
     }
 
     // TODO: need to implement this
--- src/rest/RouteHandler.ts
+++ src/rest/RouteHandler.ts
@@ -10,8 +10,13 @@ import QueryController from '../controller/QueryController';
 
 import {QueryRequest} from "../controller/QueryController";
 import Log from '../Util';
+import InsightFacade from "../controller/InsightFacade";
+
+
 
 export default class RouteHandler {
+
+    private static ifacade = new InsightFacade();
 
     private static datasetController = new DatasetController();
 
@@ -46,22 +51,30 @@ export default class RouteHandler {
                 let concated = Buffer.concat(buffer);
                 req.body = concated.toString('base64');
                 Log.trace('RouteHandler::postDataset(..) on end; total length: ' + req.body.length);
+                //
+                // let controller = RouteHandler.datasetController;
+                // controller.process(id, req.body).then(function (result) {
+                //     if (result) {                         //result -> fulfill(true)
+                //         res.json(204, {success:result});  //id was new
+                //         console.log("204")
+                //     }else {                               //result -> fulfill(false)
+                //         res.json(201, {success:result});  // id already existed
+                //         console.log("201")
+                //     }
+                //     Log.trace('RouteHandler::postDataset(..) - processed');
+                //     //res.json(200, {success: result});
+                // }).catch(function (err: Error) {
+                //     Log.trace('RouteHandler::postDataset(..) - ERROR: ' + err.message);
+                //     res.json(400, {error: err.message});
+                // });
 
-                let controller = RouteHandler.datasetController;
-                controller.process(id, req.body).then(function (result) {
-                    if (result) {                         //result -> fulfill(true)
-                        res.json(204, {success:result});  //id was new
-                        console.log("204")
-                    }else {                               //result -> fulfill(false)
-                        res.json(201, {success:result});  // id already existed
-                        console.log("201")
-                    }
-                    Log.trace('RouteHandler::postDataset(..) - processed');
-                    //res.json(200, {success: result});
-                }).catch(function (err: Error) {
-                    Log.trace('RouteHandler::postDataset(..) - ERROR: ' + err.message);
-                    res.json(400, {error: err.message});
+                RouteHandler.ifacade.addDataset(id, req.body).then(function (result) {
+                    res.json(result.code,result.body )
+                } ).catch(function (err) {
+                    res.json(err.code,err.body )
                 });
+
+
             });
 
         } catch (err) {
@@ -73,49 +86,63 @@ export default class RouteHandler {
 
     public static postQuery(req: restify.Request, res: restify.Response, next: restify.Next) {
         Log.trace('RouteHandler::postQuery(..) - params: ' + JSON.stringify(req.params));
-        try {
-            let query: QueryRequest = req.params;
-            let datasets: Datasets = RouteHandler.datasetController.getDatasets();
-            let controller = new QueryController(datasets);
-            let isValid = controller.isValid(query);
+        // try {
+               let query: QueryRequest = req.params;
+        //     let datasets: Datasets = RouteHandler.datasetController.getDatasets();
+        //     let controller = new QueryController(datasets);
+        //     let isValid = controller.isValid(query);
+        //
+        //     if (isValid === true) {
+        //         console.log("is valid true");
+        //         let result = controller.query(query);
+        //         if(result instanceof Array){ res.json(424, {missing: result});}
+        //         else {
+        //             res.json(200, result);
+        //         }
+        //     } else {
+        //         console.log("is valid false");
+        //         res.json(400, {error: 'invalid query'});
+        //     }
+        // } catch (err) {
+        //     Log.error('RouteHandler::postQuery(..) - ERROR: ' + err);
+        //     res.send(400,{error: 'invalid query'});
+        // }
 
-            if (isValid === true) {
-                console.log("is valid true");
-                let result = controller.query(query);
-                if(result instanceof Array){ res.json(424, {missing: result});}
-                else {
-                    res.json(200, result);
-                }
-            } else {
-                console.log("is valid false");
-                res.json(400, {error: 'invalid query'});
-            }
-        } catch (err) {
-            Log.error('RouteHandler::postQuery(..) - ERROR: ' + err);
-            res.send(400,{error: 'invalid query'});
-        }
+        RouteHandler.ifacade.performQuery(query).then(function (result) {
+           res.json(result.code,result.body )
+        } ).catch(function (err) {
+            res.json(err.code,err.body )
+        });
+
         return next();
     }
 
     // added deleteDataset
     public static  deleteDataset(req: restify.Request, res: restify.Response, next: restify.Next) {
         Log.trace('RouteHandler::deleteDataset(..) - params: ' + JSON.stringify(req.params));
-        try {
-            let id: string = req.params.id;
-            if (RouteHandler.datasetController.getDataset(id) != undefined){
-                fs.unlinkSync("./data/"+id);
-                RouteHandler.datasetController.deleteDataSets(id);
-                Log.trace('RouteHandler::deleteDataset(..) - processed');
-                res.json(204, {success: 'dataset is deleted'});
-                console.log(" delete: 204")
-            } else {
-                res.json(404, {status: 'the delete was for a resource that was not previously PUT'});
-                console.log("delete: 404")
-            }
-        } catch (err) {
-            Log.error('RouteHandler::deleteDataset(..) - ERROR: ' + err);
-            res.send(400);
-        }
+        // try {
+        //     let id: string = req.params.id;
+        //     if (RouteHandler.datasetController.getDataset(id) != undefined){
+        //         fs.unlinkSync("./data/"+id);
+        //         RouteHandler.datasetController.deleteDataSets(id);
+        //         Log.trace('RouteHandler::deleteDataset(..) - processed');
+        //         res.json(204, {success: 'dataset is deleted'});
+        //         console.log(" delete: 204")
+        //     } else {
+        //         res.json(404, {status: 'the delete was for a resource that was not previously PUT'});
+        //         console.log("delete: 404")
+        //     }
+        // } catch (err) {
+        //     Log.error('RouteHandler::deleteDataset(..) - ERROR: ' + err);
+        //     res.send(400);
+        // }
+        RouteHandler.ifacade.removeDataset(req.params.id).then(function (result) {
+            res.json(result.code,result.body )
+        } ).catch(function (err) {
+            res.json(err.code,err.body )
+        });
+
+
         return next();
     }
 }
